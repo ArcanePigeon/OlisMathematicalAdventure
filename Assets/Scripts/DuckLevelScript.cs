99c860684afc6b2c4222c1b76876905d67ed9cb1
Reilly Gregorio <rgregor@ncsu.edu>
2021-04-15 22:41:54 -0400

    added scrolling items, foliage, added items spawning randomly, adjuested booster times and speed, added buttons to spawn items yourself

--- Assets/Scripts/DuckLevelScript.cs
+++ Assets/Scripts/DuckLevelScript.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 using TMPro;
 
 public class DuckLevelScript : MonoBehaviour {
+    public enum Items{
+        LilyPad,
+        Rock,
+        Bread,
+        HealthCrate,
+        BoosterCrate,
+        Tree,
+        Bush
+    }
     public GameObject duck;
     public int currentLane = 0;
     public GameObject[] lane = new GameObject[4];
@@ -12,17 +21,31 @@ public class DuckLevelScript : MonoBehaviour {
     public GameObject foliage1;
     public GameObject foliage2;
     public GameObject answer;
-    public GameObject lilyPad;
-    public GameObject healthCrate;
-    public GameObject boosterCrate;
     public GameObject rocketBooster;
+    public List<GameObject> lilyPadPool = new List<GameObject>();
+    public GameObject lilyPad;
+    public int numLilyPads = 5;
+    public List<GameObject> healthCratePool = new List<GameObject>();
+    public GameObject healthCrate;
+    public int numHealthCrates = 2;
+    public List<GameObject> boosterCratePool = new List<GameObject>();
+    public GameObject boosterCrate;
+    public int numRocketBoosters = 1;
+    public List<GameObject> rockPool = new List<GameObject>();
     public GameObject rock;
+    public int numRocks = 5;
+    public List<GameObject> breadPool = new List<GameObject>();
+    public GameObject bread;
+    public int numBread = 5;
     public TMP_Text questionTMP;
     public TMP_Text scoreTMP;
     public int health = 6;
     public int score = 0;
     public string question = "5X5";
     public float moveSpeed = 5f;
+    public float speed = 5f;
+    public float timeBtwSpawn = 0f;
+    public float startTimeBtwSpawn = 1f;
 
     // Start is called before the first frame update
     void Start() {
@@ -31,10 +54,42 @@ public class DuckLevelScript : MonoBehaviour {
             hearts[i].SetActive(false);
         }
         hearts[5].SetActive(true);
+        for(int i = 0; i < numLilyPads; i++){
+            GameObject obj = (GameObject)Instantiate(lilyPad);
+            obj.SetActive(false);
+            lilyPadPool.Add(obj);
+        }
+        for(int i = 0; i < numHealthCrates; i++){
+            GameObject obj = (GameObject)Instantiate(healthCrate);
+            obj.SetActive(false);
+            healthCratePool.Add(obj);
+        }
+        for(int i = 0; i < numRocketBoosters; i++){
+            GameObject obj = (GameObject)Instantiate(boosterCrate);
+            obj.SetActive(false);
+            boosterCratePool.Add(obj);
+        }
+        for(int i = 0; i < numRocks; i++){
+            GameObject obj = (GameObject)Instantiate(rock);
+            obj.SetActive(false);
+            rockPool.Add(obj);
+        }
+        for(int i = 0; i < numBread; i++){
+            GameObject obj = (GameObject)Instantiate(bread);
+            obj.SetActive(false);
+            breadPool.Add(obj);
+        }
     }
 
     // Update is called once per frame
     void Update() {
+        if(timeBtwSpawn <= 0){
+            Items i = Random.Range(0,101) < 95 ? (Items)(Random.Range(0,3)) : (Items)Random.Range(3,5);
+            SpawnLane(i,Random.Range(0,4));
+            timeBtwSpawn = startTimeBtwSpawn;
+        }else{
+            timeBtwSpawn -= Time.deltaTime;
+        }
         if(Input.GetKeyDown("w")){
             if(currentLane >= 1){
                 currentLane--;
@@ -45,8 +100,93 @@ public class DuckLevelScript : MonoBehaviour {
                 currentLane++;
             }
         }
+        if(Input.GetKeyDown("1")){
+            SpawnLane(Items.LilyPad, 0);
+        }
+        if(Input.GetKeyDown("2")){
+            SpawnLane(Items.HealthCrate, 1);
+        }
+        if(Input.GetKeyDown("3")){
+            SpawnLane(Items.BoosterCrate, 2);
+        }
+        if(Input.GetKeyDown("4")){
+            SpawnLane(Items.Rock, 3);
+        }
         if(duck.transform.position.y != duckLanes[currentLane].transform.position.y){
             duck.transform.position = Vector2.Lerp(duck.transform.position,duckLanes[currentLane].transform.position,Time.deltaTime*moveSpeed);
+        }
+    }
+    public void SpawnLane(Items item, int l){
+        if(item == Items.LilyPad){
+            for(int i = 0; i < numLilyPads; i++){
+                if(!lilyPadPool[i].activeInHierarchy){
+                    GameObject obj = lilyPadPool[i];
+                    obj.transform.position = lane[l].transform.position;
+                    obj.SetActive(true);
+                    break;
+                }else{
+                    GameObject obj = (GameObject)Instantiate(lilyPad);
+                    obj.SetActive(false);
+                    lilyPadPool.Add(obj);
+                    numLilyPads++;
+                }
+            }
+        }else if(item == Items.HealthCrate){
+            for(int i = 0; i < numHealthCrates; i++){
+                if(!healthCratePool[i].activeInHierarchy){
+                    GameObject obj = healthCratePool[i];
+                    obj.transform.position = lane[l].transform.position;
+                    obj.SetActive(true);
+                    break;
+                }else{
+                    GameObject obj = (GameObject)Instantiate(healthCrate);
+                    obj.SetActive(false);
+                    healthCratePool.Add(obj);
+                    numHealthCrates++;
+                }
+            }
+        }else if(item == Items.BoosterCrate){
+            for(int i = 0; i < numRocketBoosters; i++){
+                if(!boosterCratePool[i].activeInHierarchy){
+                    GameObject obj = boosterCratePool[i];
+                    obj.transform.position = lane[l].transform.position;
+                    obj.SetActive(true);
+                    break;
+                }else{
+                    GameObject obj = (GameObject)Instantiate(boosterCrate);
+                    obj.SetActive(false);
+                    boosterCratePool.Add(obj);
+                    numRocketBoosters++;
+                }
+            }
+        }else if(item == Items.Rock){
+            for(int i = 0; i < numRocks; i++){
+                if(!rockPool[i].activeInHierarchy){
+                    GameObject obj = rockPool[i];
+                    obj.transform.position = lane[l].transform.position;
+                    obj.SetActive(true);
+                    break;
+                }else{
+                    GameObject obj = (GameObject)Instantiate(rock);
+                    obj.SetActive(false);
+                    rockPool.Add(obj);
+                    numRocks++;
+                }
+            }
+        }else if(item == Items.Bread){
+            for(int i = 0; i < numBread; i++){
+                if(!breadPool[i].activeInHierarchy){
+                    GameObject obj = breadPool[i];
+                    obj.transform.position = lane[l].transform.position;
+                    obj.SetActive(true);
+                    break;
+                }else{
+                    GameObject obj = (GameObject)Instantiate(bread);
+                    obj.SetActive(false);
+                    breadPool.Add(obj);
+                    numBread++;
+                }
+            }
         }
     }
 
@@ -55,31 +195,44 @@ public class DuckLevelScript : MonoBehaviour {
         // if Oli collides with obstacles (rock/lily pad), hide obstacle
         //  then decrease his health by 1 (half heart value)
         //  and call SetHeartView with new health
-        if (other.gameObject.tag == "Obstacle") {
+        if (other.CompareTag("Obstacle")) {
             other.gameObject.SetActive(false);
             health -= 1;
+            if(health < 0){
+                health = 0;
+            }
             SetHeartView(health);
         }
 
         // if Oli collides with a health boost crate, hide crate
         //  then increase his health by 2 (full heart value)
         //  and call SetHeartView with new health
-        if (other.gameObject == healthCrate) {
+        if (other.CompareTag("HeartCrate")) {
             other.gameObject.SetActive(false);
             health += 2;
+            if(health > 6){
+                health = 6;
+            }
             SetHeartView(health);
+        }
+
+        if (other.CompareTag("Money")) {
+            other.gameObject.SetActive(false);
+            score += 1000;
         }
 
         // if Oli collides with a jet pack crate, hide crate
         //  then start a 5 sec interval, set jet pack to active and increase
         //  speed. After 5 sec set jet pack inactive and reduce to normal speed
-        if (other.gameObject == boosterCrate) {
+        if (other.CompareTag("BoosterCrate") && !rocketBooster.activeInHierarchy) {
             other.gameObject.SetActive(false);
             rocketBooster.SetActive(true);
-            moveSpeed += 4f;
-            yield return new WaitForSeconds(15f);
+            moveSpeed += 5f;
+            speed += 5f;
+            yield return new WaitForSeconds(5f);
             rocketBooster.SetActive(false);
-            moveSpeed -= 4f;
+            moveSpeed -= 5f;
+            speed -= 5f;
         }
     }
 
--- Assets/Scripts/ScrollingScript.cs
+++ Assets/Scripts/ScrollingScript.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScrollingScript : MonoBehaviour {
-    public GameObject obstacle;
-    public float velocity = 5f;
+    public GameObject duck;
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start() {
+        duck = GameObject.FindGameObjectsWithTag("Duck")[0];
+        speed = duck.GetComponent<DuckLevelScript>().speed;
     }
 
     // Update is called once per frame
     void Update() {
+        speed = duck.GetComponent<DuckLevelScript>().speed;
+        transform.Translate(Vector2.left*speed*Time.deltaTime);
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("BackWall")){
+            gameObject.SetActive(false);
+        }
     }
 }
